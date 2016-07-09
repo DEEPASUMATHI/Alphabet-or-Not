e1781d345ee58cfcd1ff5893d45fee912b4bcb7b
DEEPASUMATHI <deepasumathi114@gmail.com>
2016-07-09 10:29:38 +0530

    Create alp.c

---
+++ alp.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char a;
+scanf("%c",&a);
+if(((a>='a')&&(a<='z'))||((a>='A')&&(a<='Z')))
+ printf("Alphabet");
+else
+ printf("Not an alphabet");
+return 0;
+}
